fde76b9bba0ce2230961807efa69ff760abe83d9
ujoutr <penguin@inpane.com>
2021-09-13 13:26:52 +0900

    座標ツイート機能追加

--- js/openseadragon-nav.js
+++ js/openseadragon-nav.js
@@ -26,10 +26,37 @@
 
 	function addCoordNav(self) {
 		const p = document.createElement('div');
+
+		const tw = document.createElement('div');
+		tw.id = 'Twitterlist';
+		p.appendChild(tw);
+
+		const twbtn = document.createElement('div');
+		twbtn.id = 'Twitterbtn';
+		twbtn.innerHTML = '<a href="javascript:/*twitter*/"><i class="fab fa-twitter"></i>Twitter</a>';
+		p.appendChild(twbtn);
+
 		const p2 = document.createElement('div');
 		p2.id = 'position';
-		p.appendChild(p2);
+		twbtn.appendChild(p2);
 		self.addControl(p, { anchor: OpenSeadragon.ControlAnchor.BOTTOM_RIGHT });
+
+		jQuery("#Twitterbtn").on("click",function() {
+			let Twitterto = jQuery('.Twitterto:checked').map(function() {return jQuery(this).val();});
+			let twto = "";
+			for(let i=0; i<Twitterto.length; i++) twto += "@"+Twitterto[i]+" ";
+
+			let urlhash = location.hash.split('&');
+			let urlx = urlz = "";
+			for(let i=0; i<urlhash.length; i++) {
+				if(!urlhash[i].indexOf("x=")){urlx=urlhash[i].substr(2);}
+				if(!urlhash[i].indexOf("z=")){urlz=urlhash[i].substr(2);}
+			}
+			let twtext = twto+"〇〇〇のホロ鯖座標は、X:"+urlx+" Z:"+urlz+" です。";
+			let winurl = "https://twitter.com/intent/tweet?text="+encodeURI(twtext)+"%0D%0A&url="+encodeURI(location.href);
+			window.open(winurl, '_blank');
+			return false;
+		});
 
 		const tracker = new OpenSeadragon.MouseTracker({
 			element: self.container, 
@@ -50,5 +77,8 @@
 
 		tracker.setTracking(true);
 	}
+
+
+
 
 })();
